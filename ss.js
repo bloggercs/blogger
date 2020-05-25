8e2be9cf9d1a0450b92d9ee272505d9c3abcc462
bloggercs <54446304+bloggercs@users.noreply.github.com>
2020-05-25 21:16:14 +0300

    Update ss.js

--- ss.js
+++ ss.js
@@ -6,7 +6,12 @@ await sleep(1000);
 if(document.getElementById("top_header_addone")){
 	var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '//ad.a-ads.com/1396857?size=728x90';
+    script.text = "atOptions = {'key' : '1da037bc0d05b04ca5a97f68953da67b','format' : 'iframe','height' : 90,'width' : 728,'params' : {} 	};";
+	var el = document.getElementById("top_header_addone");
+    el.appendChild(script);
+	var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://yydtbpms8tf4.com/1da037bc0d05b04ca5a97f68953da67b/invoke.js';
     var el = document.getElementById("top_header_addone");
     el.appendChild(script);
 }
@@ -14,7 +19,12 @@ await sleep(3000);
 if(document.getElementById("sidebar_add")){
 	var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '//ad.a-ads.com/1396854?size=300x250';
+    script.text = "atOptions = {'key' : '75aa95e7c0c853dc23abd68b8b5e72ac','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
+	var el = document.getElementById("sidebar_add");
+    el.appendChild(script);
+	var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://yydtbpms8tf4.com/75aa95e7c0c853dc23abd68b8b5e72ac/invoke.js';
     var el = document.getElementById("sidebar_add");
     el.appendChild(script);
 }
@@ -22,7 +32,12 @@ await sleep(3000);
 if(document.getElementById("center_middle_add")){
 	var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '/ad.a-ads.com/1396856?size=468x60';
+    script.text = "atOptions = {'key' : 'f52e10fa10bb64c2eba8b5c95eb84d5a','format' : 'iframe','height' : 60,'width' : 468,'params' : {} 	};";
+	var el = document.getElementById("center_middle_add");
+    el.appendChild(script);
+	var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://yydtbpms8tf4.com/f52e10fa10bb64c2eba8b5c95eb84d5a/invoke.js';
     var el = document.getElementById("center_middle_add");
     el.appendChild(script);
 }
