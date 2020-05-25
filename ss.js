0f5a1be3fb52218e563b4884ea2e2ba800561bd7
bloggercs <54446304+bloggercs@users.noreply.github.com>
2020-05-25 19:32:20 +0300

    Update ss.js

--- ss.js
+++ ss.js
@@ -4,42 +4,24 @@ function sleep(ms) {
 async function derot() {
 await sleep(1000);
 if(document.getElementById("top_header_addone")){
-	var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.text = "atOptions = {'key' : '1da037bc0d05b04ca5a97f68953da67b','format' : 'iframe','height' : 90,'width' : 728,'params' : {} 	};";
-	var el = document.getElementById("top_header_addone");
-    el.appendChild(script);
-	var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'yydtbpms8tf4.com/1da037bc0d05b04ca5a97f68953da67b/invoke.js';
-    var el = document.getElementById("top_header_addone");
-    el.appendChild(script);
+	var x = document.createElement("center");
+x.style = 'margin-top: 5px';
+x.innerHTML = '<iframe data-aa="1396857" src="//ad.a-ads.com/1396857?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>';
+document.body.appendChild(x);
 }
 await sleep(3000);
 if(document.getElementById("sidebar_add")){
-	var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.text = "atOptions = {'key' : '75aa95e7c0c853dc23abd68b8b5e72ac','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
-	var el = document.getElementById("sidebar_add");
-    el.appendChild(script);
-	var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'yydtbpms8tf4.com/75aa95e7c0c853dc23abd68b8b5e72ac/invoke.js';
-    var el = document.getElementById("sidebar_add");
-    el.appendChild(script);
+	var x = document.createElement("center");
+x.style = 'margin-top: 5px';
+x.innerHTML = '<iframe data-aa="1396854" src="//ad.a-ads.com/1396854?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>';
+document.body.appendChild(x);
 }
 await sleep(3000);
 if(document.getElementById("center_middle_add")){
-	var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.text = "atOptions = {'key' : 'f52e10fa10bb64c2eba8b5c95eb84d5a','format' : 'iframe','height' : 60,'width' : 468,'params' : {} 	};";
-	var el = document.getElementById("center_middle_add");
-    el.appendChild(script);
-	var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'yydtbpms8tf4.com/f52e10fa10bb64c2eba8b5c95eb84d5a/invoke.js';
-    var el = document.getElementById("center_middle_add");
-    el.appendChild(script);
+	var x = document.createElement("center");
+x.style = 'margin-top: 5px';
+x.innerHTML = '<iframe data-aa="1396856" src="//ad.a-ads.com/1396856?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>';
+document.body.appendChild(x);
 }
 }
 derot();
