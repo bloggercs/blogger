d2c3702029b2f64287dc25fbc89dd91843ce5720
bloggercs <54446304+bloggercs@users.noreply.github.com>
2020-05-25 20:01:23 +0300

    Update ss.js

--- ss.js
+++ ss.js
@@ -6,7 +6,7 @@ await sleep(1000);
 if(document.getElementById("top_header_addone")){
 	var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '<iframe data-aa="1396857" src="//ad.a-ads.com/1396857?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>';
+    script.src = '//ad.a-ads.com/1396857?size=728x90';
     var el = document.getElementById("top_header_addone");
     el.appendChild(script);
 }
@@ -14,7 +14,7 @@ await sleep(3000);
 if(document.getElementById("sidebar_add")){
 	var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '<iframe data-aa="1396854" src="//ad.a-ads.com/1396854?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>';
+    script.src = '//ad.a-ads.com/1396854?size=300x250';
     var el = document.getElementById("sidebar_add");
     el.appendChild(script);
 }
@@ -22,7 +22,7 @@ await sleep(3000);
 if(document.getElementById("center_middle_add")){
 	var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '<iframe data-aa="1396856" src="//ad.a-ads.com/1396856?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>';
+    script.src = '/ad.a-ads.com/1396856?size=468x60';
     var el = document.getElementById("center_middle_add");
     el.appendChild(script);
 }
