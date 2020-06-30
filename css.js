585bc561f45a4672279f33403ba367470585a646
bloggercs <54446304+bloggercs@users.noreply.github.com>
2020-06-30 11:06:00 +0300

    Update css.js

--- css.js
+++ css.js
@@ -6,6 +6,31 @@
     document.body.appendChild(script);
 })();
 (function() {
+    var configuration = {
+    "token": "0fc4c33227878b02768b61b867a4e64d",
+    "entryScript": {
+        "type": "timeout",
+        "timeout": 3000,
+        "capping": {
+            "limit": 5,
+            "timeout": 24
+        }
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+(function() {
  var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = '//yydtbpms8tf4.com/37/1e/86/371e86f062c27858659802d31bb64f81.js';
