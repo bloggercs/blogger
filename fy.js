cafe3bca50ede37b574df68bb855d07706ca1805
bloggercs <54446304+bloggercs@users.noreply.github.com>
2021-08-09 23:41:25 +0300

    Update fy.js

--- fy.js
+++ fy.js
@@ -1,1 +1,8 @@
-
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4437456,document.body||document.documentElement)
+(function() {
+ var script = document.createElement('script');
+    script.async = "async";
+    script.type = 'text/javascript';
+    script.src = 'https://inpage-push.com/400/4437459';
+    document.body.appendChild(script);
+})();
