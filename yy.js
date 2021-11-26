5891da55cf90f8d85dbb2de860e9b841843f67e2
bloggercs <54446304+bloggercs@users.noreply.github.com>
2021-11-27 01:24:12 +0300

    Update yy.js

--- yy.js
+++ yy.js
@@ -41,5 +41,28 @@ if(document.getElementById("center_middle_add")){
     var el = document.getElementById("center_middle_add");
     el.appendChild(script);
 }
-}
-derot(); 
+(function() {
+    var configuration = {
+    "token": "0fc4c33227878b02768b61b867a4e64d",
+    "entryScript": {
+        "type": "timeout",
+        "timeout": 3000,
+        "capping": {
+            "limit": 5,
+            "timeout": 24
+        }
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
