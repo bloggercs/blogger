8173dddb5ec2cf065e1a399b18860350e2ca7743
bloggercs <54446304+bloggercs@users.noreply.github.com>
2022-01-02 04:34:58 +0300

    Update yy.js

--- yy.js
+++ yy.js
@@ -52,28 +52,4 @@ document.getElementById("and_botton_ads_46860").innerHTML='<x>';
 if(document.getElementById("and_botton_ads_300250")){
 document.getElementById("and_botton_ads_300250").innerHTML='<x>';
 }
-(function() {
-    var configuration = {
-    "token": "0fc4c33227878b02768b61b867a4e64d",
-    "entryScript": {
-        "type": "timeout",
-        "timeout": 3000,
-        "capping": {
-            "limit": 5,
-            "timeout": 24
-        }
-    },
-    "exitScript": {
-        "enabled": true
-    },
-    "popUnder": {
-        "enabled": true
-    }
-};
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = '//cdn.shorte.st/link-converter.min.js';
-    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
-    var entry = document.getElementsByTagName('script')[0];
-    entry.parentNode.insertBefore(script, entry);
 })();
