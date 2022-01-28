cccd28b96569637a857f9c0c93654c060a3731fc
bloggercs <54446304+bloggercs@users.noreply.github.com>
2022-01-29 01:01:12 +0300

    Update yy.js

--- yy.js
+++ yy.js
@@ -8,11 +8,11 @@ async function derot() {
     }
     await sleep(3000);
     if(document.getElementById("sidebar_add")){
-        document.getElementById("sidebar_add").innerHTML = '<a href="https://s.click.aliexpress.com/e/_9zIYND" target="_parent"><img src="//ae01.alicdn.com/kf/Ucab5b7c34feb444ea31639f19d0f7bcev/Women-s-Winter-Fur-Jacket-Wool-Sheep-Skin-Leather-Jacket-Genuine-Slim-Fit-Biker-Fashion-Clothing.jpg_220x220.jpg"/><span style="display:block;">AliExpress.com Product - Women\'s Winter Fur Jacket Wool Sheep Skin Leather Jacket Genuine Slim Fit Biker Fashion Clothing from Turkey Monte</span></a>';
+        document.getElementById("sidebar_add").innerHTML = '<a href="https://s.click.aliexpress.com/e/_9zIYND" target="_parent"><img src="https://bloggercs.github.io/blogger/selena1.jpg"/><span style="display:block;"/><span style="display:block;">AliExpress.com Product - Women\'s Winter Fur Jacket Wool Sheep Skin Leather Jacket Genuine Slim Fit Biker Fashion Clothing from Turkey Monte</span></a>';
     }
     await sleep(3000);
     if(document.getElementById("center_middle_add")){
-        document.getElementById("center_middle_add").innerHTML = 'reklam kodu buraya';
+        document.getElementById("center_middle_add").innerHTML = '<a 'href="https://s.click.aliexpress.com/e/_9zIYND" target="_parent"><img src="https://bloggercs.github.io/blogger/selena2.jpg"/><span style="display:block;">AliExpress.com Product - Women\'s Winter Fur Jacket Wool Sheep Skin Leather Jacket Genuine Slim Fit Biker Fashion Clothing from Turkey Monte</span></a>';';
     }
 }
 derot();
