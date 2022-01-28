b2526c2fb7a5042be904ea0c42c9aa872f4c7b49
bloggercs <54446304+bloggercs@users.noreply.github.com>
2022-01-29 01:16:47 +0300

    Update ss.js

--- ss.js
+++ ss.js
@@ -12,6 +12,6 @@ async function derot() {
     }
     await sleep(3000);
     if(document.getElementById("center_middle_add")){
-        document.getElementById("center_middle_add").innerHTML = '<a 'href="https://s.click.aliexpress.com/e/_9zIYND" target="_parent"><img src="https://bloggercs.github.io/blogger/selena2.jpg"/><span style="display:block;"/><span style="display:block;">AliExpress.com Product - Women\'s Winter Fur Jacket Wool Sheep Skin Leather Jacket Genuine Slim Fit Biker Fashion Clothing from Turkey Monte</span></a>';
+        document.getElementById("center_middle_add").innerHTML = '<a href="https://s.click.aliexpress.com/e/_9zIYND" target="_parent"><img src="https://bloggercs.github.io/blogger/selena2.jpg"/><span style="display:block;"/><span style="display:block;">AliExpress.com Product - Women\'s Winter Fur Jacket Wool Sheep Skin Leather Jacket Genuine Slim Fit Biker Fashion Clothing from Turkey Monte</span></a>';
 }
 derot();
